cc4a73c833e29ab2a7085d0bcd6b586098c30045
luskafaria <lucashpharia@gmail.com>
2020-06-15 00:05:38 -0300

    cart subtotal added

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import GlobalStyles from './styles/global';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
+
 import { Provider } from 'react-redux';
+
 import CartContainer from './containers/CartContainer';
 import SearchContainer from './containers/SearchContainer';
+
 import { PersistGate } from 'redux-persist/integration/react';
 
 import { store, persistor } from './store';
